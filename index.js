627876f07791b900e1df4bdac562a9da7dddc117
Oyinade3 <oyinadehillary@gmail.com>
2022-06-23 16:41:09 +0100

    javascript code
    code to print out name, height and country on screen

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var myName = 'Oyinade';
 var myHeight = '5 ft 2';
 var myCountry = 'Nigeria';
 
+alert('Welcome, it\'s nice to have you here \nThe link to my repository page is posted at the end of the information to be displayed.' )
 alert('Hi there, I\'m ' + myName + '.');
 alert('I\'m ' + myHeight + ' tall.');
 alert('I\'m from ' + myCountry + '.');
