2af32cff2359fbf41a7a2dc8f8414cebc72c3aaf
Oyinade3 <oyinadehillary@gmail.com>
2022-06-23 16:36:06 +0100

    my first javascript code

---
+++ index.js
@@ -0,0 +1,7 @@
+var myName = 'Oyinade';
+var myHeight = '5 ft 2';
+var myCountry = 'Nigeria';
+
+alert('Hi there, I\'m ' + myName + '.');
+alert('I\'m ' + myHeight + ' tall.');
+alert('I\'m from ' + myCountry + '.');
